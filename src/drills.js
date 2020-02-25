a6655e226ac1951d0be1eefad5326a96bdbf1075
Ron Martin <ronmartin89@gmail.com>
2020-02-25 12:01:51 -0800

    Completed drill 4

--- src/drills.js
+++ src/drills.js
@@ -36,4 +36,34 @@ function getPaginatedItems(pageNumber=1) {
     });
 }
 
-getPaginatedItems();
+//getPaginatedItems();
+
+// Drill 3
+
+function itemsAddedAfterDate(daysAgo) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where(
+      'date_added', 
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
+    .then(result => console.log(result)); 
+}
+
+//itemsAddedAfterDate(2);
+
+
+// Drill 4 
+
+function getTotalCost() {
+  // SELECT sum(price), category FROM shopping_list GROUP BY category
+  knexInstance
+    .select('category')
+    .sum('price AS total_price') 
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => console.log(result));
+}
+
+getTotalCost();
