7a3d09c8a876c938f7a593d240a413f25db51f14
Ron Martin <ronmartin89@gmail.com>
2020-02-25 09:07:42 -0800

    first commit

---
+++ test/setup.js
@@ -0,0 +1,7 @@
+// This file is run by our npm test, in package.json
+// A feature of mocha lets us require a specific setup file
+const { expect } = require('chai');
+const supertest = require('supertest');
+
+global.expect = expect;
+global.supertest = supertest;
